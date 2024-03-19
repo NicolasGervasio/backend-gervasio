6a5b5e8463654cd8b41f3c2245771db4eaba7ecb
Nicolas Gervasio <Nicolasgervasio@outlook.com>
2024-03-19 14:42:09 -0300

    modifico

--- testing.js
+++ testing.js
@@ -7,12 +7,13 @@ console.log(producto.addProduct('iPhone 13', 'Apple iPhone 13', 990, 'https://ht
 console.log(producto.addProduct('HyperX Armada 27', 'Monitor 165Hz', 450, 'https://thotcomputacion.com.uy/wp-content/uploads/2023/05/Armada_25_Stand39405_6f8b916eaea440b89c908bba697a5b07-600x600.webp', '968', '27'));
 console.log(producto.addProduct('Razer Hunstman V2', 'Teclado mecanico gamer', 198, 'https://thumb.pccomponentes.com/w-530-530/articles/36/362429/1251-razer-hunstsman-v2-analog-teclado-gaming-rgb-switch-optico-analogico-review.jpg', '711', '15')); */
 
-/* console.log(producto.getProducts()); */
-
 /* console.log(producto.deleteProduct(4)); */
 
 /* const productoActualizar = {
-        "price": 890,
+        "title": "iPhone 14",
+        "description": "Apple iPhone 14",
+        "price": 1400,
+        "thumbnail": "https://i.blogs.es/be13bf/1366_2000/500_500.jpeg",
         "stock": "10"
 }
 
