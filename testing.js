e126460040015ddaeebbe2de19c973516861cc04
Nicolas Gervasio <Nicolasgervasio@outlook.com>
2024-03-19 14:36:11 -0300

    segunda pre entrega

---
+++ testing.js
@@ -0,0 +1,19 @@
+const ProductManager = require ("./productManager.js");
+
+const producto = new ProductManager();
+
+/* console.log(producto.addProduct('ROG Phone 6 Pro', 'Celular ASUS', 2000, 'https://dlcdnwebimgs.asus.com/gain/1A2622B0-28FA-447A-9F20-88E02A441EEE/w1000/h732', '156', '10'));
+console.log(producto.addProduct('iPhone 13', 'Apple iPhone 13', 990, 'https://http2.mlstatic.com/D_NQ_NP_973345-MLA47781591382_102021-O.webp', '698', '16'));
+console.log(producto.addProduct('HyperX Armada 27', 'Monitor 165Hz', 450, 'https://thotcomputacion.com.uy/wp-content/uploads/2023/05/Armada_25_Stand39405_6f8b916eaea440b89c908bba697a5b07-600x600.webp', '968', '27'));
+console.log(producto.addProduct('Razer Hunstman V2', 'Teclado mecanico gamer', 198, 'https://thumb.pccomponentes.com/w-530-530/articles/36/362429/1251-razer-hunstsman-v2-analog-teclado-gaming-rgb-switch-optico-analogico-review.jpg', '711', '15')); */
+
+/* console.log(producto.getProducts()); */
+
+/* console.log(producto.deleteProduct(4)); */
+
+/* const productoActualizar = {
+        "price": 890,
+        "stock": "10"
+}
+
+console.log(producto.updateProduct(4, productoActualizar)); */
